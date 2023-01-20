7de3c8c8a447e21e51f94f9920e579f102823035
Frank Du <jinyangd@umich.edu>
2023-01-19 23:16:19 -0500

    updated

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
 
+    override func viewDidLoad() {
+        print("check")
+    }
     // #imageLiteral(resourceName: "DiceSix")
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        
+        print("started")
         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
         diceImageView1.image = diceArray.randomElement()
